6d136555c54027875ab6f3519ac440e26afe13db
Centick <utyatsky.ry@yandex.ru>
2024-11-28 15:33:57 +0300

    events done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,54 @@
             <p class="width-100">Войди в мир IT вместе с нами...</p>
         </div>
 
-        <div class="organ__container container grid grid-column jc-s ac-s gap-10">
-            <div class="organ_block width-100 grid grid-column">
+        <div class="organ__container container grid grid-column jc-s ac-s gap-25">
+            <div class="organ_block width-100 grid grid-column gap-25">
                 <h3 class="h3">Учитесь новому, создавайте, творите</h3>
-                <div class="btn">Узнать больше</div>
+                <router-link :to="{name: 'home'}" class="link btn organ_btn">Узнать больше</router-link>
             </div>
             <div class="organ_block width-100 grid grid-column"></div>
+        </div>
+
+        <div class="container grid grid-row jc-s ac-s gap-25">
+            <router-link :to="{name: 'home'}" class="link event_link events_link grid grid-row gap-10">
+                <img src="../assets/images/icon/event.svg" alt="event">
+                <div class="grid grid-column gap-10">
+                    <p class="event_time">22.06.2025</p>
+                    <h4>Собрание</h4>
+                </div>
+            </router-link>
+            <router-link :to="{name: 'home'}" class="link event_link events_link grid grid-row gap-10">
+                <img src="../assets/images/icon/event.svg" alt="event">
+                <div class="grid grid-column gap-10">
+                    <p class="event_time">22.06.2025</p>
+                    <h4>Собрание</h4>
+                </div>
+            </router-link>
+            <router-link :to="{name: 'home'}" class="link event_link events_link grid grid-row gap-10">
+                <img src="../assets/images/icon/event.svg" alt="event">
+                <div class="grid grid-column gap-10">
+                    <p class="event_time">22.06.2025</p>
+                    <h4>Собрание</h4>
+                </div>
+            </router-link>
+            <router-link :to="{name: 'home'}" class="link event_link events_link grid grid-row gap-10">
+                <img src="../assets/images/icon/event.svg" alt="event">
+                <div class="grid grid-column gap-10">
+                    <p class="event_time">22.06.2025</p>
+                    <h4>Собрание</h4>
+                </div>
+            </router-link>
+            <router-link :to="{name: 'home'}" class="link event_link events_link grid grid-row gap-10">
+                <img src="../assets/images/icon/event.svg" alt="event">
+                <div class="grid grid-column gap-10">
+                    <p class="event_time">22.06.2025</p>
+                    <h4>Собрание</h4>
+                </div>
+            </router-link>
+        </div>
+
+        <div class="organ__container container grid grid-column jc-s ac-s gap-25">
+            
         </div>
     </div>
 
@@ -36,4 +78,28 @@
         color: #fff;
         border-radius: 20px;
     }
-</style>
+
+    .organ_btn{
+        background-color: white;
+        color: black;
+    }
+
+    .events_link{
+        padding: 20px;
+        background-color: white;
+        border-radius: 15px;
+    }
+
+    .events_link > div{
+        gap: 5px;
+    }
+
+    .event_time{
+        font-size: 14px;
+        color: grey;
+    }
+
+    .event_link > img{
+        height: 100%;
+    }
+</style>    
