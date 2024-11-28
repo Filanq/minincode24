5df062d2521d3bddf22a9c0818d9a72192a36595
Centick <utyatsky.ry@yandex.ru>
2024-11-28 14:54:36 +0300

    + btn for style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="section__home section grid grid-column gap-25px ac-s jc-s">
-        <div class="container ">
-            <h1 class="h3">Здравствуйте, Name!</h1>
-            <p></p>
+    <div class="section grid grid-column ac-s gap-40">
+        <div class="container grid grid-column jc-s ai-s">
+            <h2 class="h2 mb-10">Здравствуйте, Name!</h2>
+            <p class="width-100">Войди в мир IT вместе с нами...</p>
+        </div>
+
+        <div class="organ__container container grid grid-column jc-s ac-s gap-10">
+            <div class="organ_block width-100 grid grid-column">
+                <h3 class="h3">Учитесь новому, создавайте, творите</h3>
+                <div class="btn">Узнать больше</div>
+            </div>
+            <div class="organ_block width-100 grid grid-column"></div>
         </div>
     </div>
+
+
 </template>
 
 <script setup>
@@ -12,7 +22,18 @@
 </script>
 
 <style scoped>
-    .container{
-        width: 200%;
+    h2{
+        text-align: start;
+    }
+
+    .organ__container{
+        grid-template-columns: 2fr 1fr;
+    }
+
+    .organ_block{
+        padding: 30px;
+        background-color: var(--colorBlueLight);
+        color: #fff;
+        border-radius: 20px;
     }
 </style>
