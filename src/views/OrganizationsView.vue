a0c921c90174bbaf53f744106fd7862e06d049ef
Filan <fedosenko009@mail.ru>
2024-11-28 14:44:08 +0300

    Organizations View Added✔

---
+++ src/views/OrganizationsView.vue
@@ -0,0 +1,34 @@
+<template>
+    <main>
+        <section class="section organization__section">
+            <div class="container organizations__container">
+                <h2 class="h2 w-600"></h2>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+main {
+    display: flex;
+}
+.left-part {
+    width: 50%;
+    height: 100%;
+    background-color: var(--colorWhite);
+}
+.right-part {
+    width: 50%;
+    height: 100%;
+    background-color: var(--colorGrey);
+}
+.right-part > img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
