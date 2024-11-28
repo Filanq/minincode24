e9c9ab791f037e76f29495bc47b26f118b3d37b3
Centick <utyatsky.ry@yandex.ru>
2024-11-28 21:28:33 +0300

    banner home

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,44 +1,177 @@
 <template>
-    
+    <div class="section header__section">
+        <div class="container header__container">
+            <nav class="header_nav" :class="{'header_nav--open': isBurgerOpen}">
+                <div>
+                    <span class="header-cursor" :class="{hideHeaderCursor: !headerCursor1.active}" :style="{ width: `${headerCursor1.width}px`, height: `${headerCursor1.height}px`, left: `${headerCursor1.left}px` }"></span>
+                    <a class="link header_link header-section-1" @click="getClickFromHeader($event, anchors.homeAnch)" ref="homeHeaderBtn" href="#home">Главная</a>
+                    <a class="link header_link header-section-1" @click="getClickFromHeader($event, anchors.aboutAnch)" ref="aboutHeaderBtn" href="#about">О нас</a>
+                    <a class="link header_link header-section-1" @click="getClickFromHeader($event, anchors.resumeAnch)" ref="resumeHeaderBtn" href="#resume">Резюме</a>
+                </div>
+                <a class="link header_link--logo" href="#">MininCode</a>
+                <div>
+                    <span class="header-cursor" :class="{hideHeaderCursor: !headerCursor2.active}" :style="{ width: `${headerCursor2.width}px`, height: `${headerCursor2.height}px`, left: `${headerCursor2.left}px` }"></span>
+                    <a class="link header_link header-section-2" @click="getClickFromHeader($event, anchors.historyAnch)" ref="historyHeaderBtn" href="#history">Истоки</a>
+                    <a class="link header_link header-section-2" @click="getClickFromHeader($event, anchors.projectsAnch)" ref="projectsHeaderBtn" href="#projects">Проекты</a>
+                    <a class="link header_link header-section-2" @click="getClickFromHeader($event, anchors.contactsAnch)" ref="contactsHeaderBtn" href="#contacts">Контакты</a>
+                </div>
+                <!-- <span class="burger_menu" @click="isBurgerOpen = !isBurgerOpen"></span> -->
+            </nav>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
     import { RouterLink } from 'vue-router';
+    import {reactive, ref, defineProps, onMounted, type Ref} from 'vue';
+
+    const props = defineProps({
+        anchors: Object
+    });
+
+    // Works Cursor Position
+    const headerCursor1 = reactive({
+        width: 0,
+        height: 0,
+        left: 0,
+        active: false,
+    });
+    const headerCursor2 = reactive({
+        width: 0,
+        height: 0,
+        left: 0,
+        active: false,
+    });
+    const activeBtn: Ref<any> = ref(null);
+
+    const homeHeaderBtn: Ref<any> = ref(null);
+    const aboutHeaderBtn: Ref<any> = ref(null);
+    const resumeHeaderBtn: Ref<any> = ref(null);
+    const historyHeaderBtn: Ref<any> = ref(null);
+    const projectsHeaderBtn: Ref<any> = ref(null);
+    const contactsHeaderBtn: Ref<any> = ref(null);
+
+    function scrollTo(view: Ref<HTMLElement | null>) { 
+        view.value?.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    let timerId: ReturnType<typeof setTimeout>;
+
+    function getClickFromHeader(headerBtnEvent: any, scrollElement: any) {
+        headerBtnEvent.preventDefault();
+        clearTimeout(timerId);
+        scrollCheckerActive = false;
+        setHeaderCursor(headerBtnEvent.target);
+        if (scrollElement) scrollTo(scrollElement);
+        timerId = setTimeout(() => {
+            scrollCheckerActive = true;
+            scrollChecker();
+        }, 1000);
+    }
+
+    // Change Header Cursor And Current Section on Click
+    function setHeaderCursor(element: HTMLElement): void {
+        activeBtn.value = element;
+        if (element.classList.contains('header-section-1')) {
+            if (headerCursor1.active) {
+                headerCursor1.width = element.offsetWidth;
+                headerCursor1.height = element.offsetHeight;
+                headerCursor1.left = element.offsetLeft;
+            } else {
+                headerCursor1.active = true;
+                headerCursor2.active = false;
+                headerCursor1.width = element.offsetWidth;
+                headerCursor1.height = element.offsetHeight;
+                headerCursor1.left = element.offsetLeft;
+                headerCursor2.width = element.offsetWidth;
+                headerCursor2.height = element.offsetHeight;
+                headerCursor2.left = element.offsetLeft;
+            }
+        } else if (element.classList.contains('header-section-2')) {
+            if (headerCursor2.active) {
+                headerCursor2.width = element.offsetWidth;
+                headerCursor2.height = element.offsetHeight;
+                headerCursor2.left = element.offsetLeft;
+            } else {
+                headerCursor2.active = true;
+                headerCursor1.active = false;
+                headerCursor2.width = element.offsetWidth;
+                headerCursor2.height = element.offsetHeight;
+                headerCursor2.left = element.offsetLeft;
+                headerCursor1.width = element.offsetWidth;
+                headerCursor1.height = element.offsetHeight;
+                headerCursor1.left = element.offsetLeft;
+            }
+        }
+    }
+
+    let scrollCheckerActive = true;
+
+    const scrollChecker = () => {
+        if (scrollCheckerActive) {
+            if (window.pageYOffset >= props.anchors.contactsAnch.value.offsetTop) {
+                setHeaderCursor(contactsHeaderBtn.value);
+            } else if (window.pageYOffset >= props.anchors.projectsAnch.value.offsetTop) {
+                setHeaderCursor(projectsHeaderBtn.value);
+            } else if (window.pageYOffset >= props.anchors.historyAnch.value.offsetTop) {
+                setHeaderCursor(historyHeaderBtn.value);
+            } else if (window.pageYOffset >= props.anchors.resumeAnch.value.offsetTop) {
+                setHeaderCursor(resumeHeaderBtn.value);
+            } else if (window.pageYOffset >= props.anchors.aboutAnch.value.offsetTop) {
+                setHeaderCursor(aboutHeaderBtn.value);
+            } else if (window.pageYOffset >= props.anchors.homeAnch.value.offsetTop) {
+                setHeaderCursor(homeHeaderBtn.value);
+            }
+        }
+    }
+
+    // On Mounted Clicks Default Cursor Object
+    onMounted(() => {
+        addEventListener("scroll", scrollChecker);
+        addEventListener("DOMContentLoaded", scrollChecker);
+        addEventListener("resize", () => setHeaderCursor(activeBtn.value));
+    })
+
+    let isBurgerOpen = ref(false)
 </script>
 
 <style scoped>
     .header__section{
-        height: 100vh;
-        background-color: white;
-        display: flex;
+        padding: 20px 0;   
+        z-index: 10;
         position: fixed;
-        left: 0;
-        top: 0;
-        z-index: 9;
-        width: 300px;
-        padding: 40px 0;
+    }
+    .header_nav{
+        padding: 8px 10px ;
+        background-color: rgb(166, 166, 166);
+        border-radius: 100px;
+        display: flex;
+        justify-content: space-between;
+        gap: 30px;
+        z-index: 10;
+        width: 100%;
     }
 
-    .header__container{
-        padding: 0 60px;
+    .header_nav div{
+        display: flex;
+        gap: 20px;
     }
 
     .header_link{
-        font-size: 18px;
+        display: flex;
+        align-items: center;
+        padding: 10px 24px;
+        border-radius: 50px;
+        z-index: 2;
     }
 
-    .logo{
-        font-size: 28px;
-        font-weight: 700;
+    .header_link--active{
+        background-color: var(--colorBlue);
     }
 
-    .header_link--red{
-        color: rgb(255, 18, 0);
+    .header_link--logo{
+        display: flex;
+        align-items: center;
+        font-size: 26px;
     }
-
-    .header_link--blue{
-        color: var(--colorBlueMain);
-    }
-
-    
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,27 @@
 <template>
-    <h1>main</h1>
+    <HeaderComponent/>
+    <div class="section banner__section grid jc-c ac-c" >
+        <div class="container grid jc-c">
+            <h1 class="h1">MININCODE</h1>
+            <!-- <img class="banner_img" src="#" alt="img"> -->
+        </div>
+    </div>
+
+    <MarqueComponent/>
 </template>
+
+<script setup>
+    import MarqueComponent from '@/components/MarqueComponent.vue';
+    import HeaderComponent from '@/components/HeaderComponent.vue';
+</script>
+
+<style scoped>
+    .banner__section{
+        width: 100vw;
+        height: 100vh;
+        position: relative;
+        z-index: 0;
+    }
+
+    
+</style>
