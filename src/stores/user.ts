7c8c52e0385928fb3bbf5fae608f66120d936dfd
Filan <fedosenko009@mail.ru>
2024-11-28 23:55:04 +0300

    user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,8 +1,25 @@
-import {Ref, ref} from 'vue'
+import {type Ref, ref} from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useUserStore = defineStore('user', () => {
   const id: Ref<number> = ref(0);
+  const type: Ref<string> = ref('');
 
-  return { id }
+  function login(){
+    axios.get(window.origin + '/api/auth/').then(res => {
+      if(res.data['result']){
+        id.value = Number(res.data['id']);
+        type.value = String(res.data['type']);
+      }
+    });
+  }
+  function logout(){
+    axios.post(window.origin + '/api/logout/').then(res => {
+      id.value = 0;
+      type.value = '';
+    });
+  }
+
+  return { id, type, login, logout };
 })
