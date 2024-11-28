f33585e70474f4aa5542d7639c6a35c8c4762799
Allderfun1 <qas22042007@gmail.com>
2024-11-28 16:36:42 +0300

    Create News and Events

---
+++ src/views/NewsView.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="section grid grid-column ac-s gap-40">
+        <div class="container">
+            <h3 class="h3 mb-10">Новости</h3>
+            <span class="mini-text">Что-то</span>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h3 {
+        align-self: start;
+    }
+    .mini-text {
+        display: inline;
+        font-weight: 300;
+        color: #858585;
+        font-size: 14px;
+    }
+</style>
